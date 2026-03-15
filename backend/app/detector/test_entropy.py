b9f687ba378433343495232ac15f4af10cfef7a2
Manasa Sankar <manasa.sanriya@gmail.com>
2026-03-15 23:05:50 +0530

    ,..

--- backend/app/detector/test_entropy.py
+++
@@ -1,4 +0,0 @@
-from entropy import calculate_entropy
-
-ip_data = ["192.168.1.2", "192.168.1.3", "192.168.1.3", "192.168.1.3"]
-print("Entropy:", calculate_entropy(ip_data))
--- backend/app/utils/firewall.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def block_ip(ip):
-    os.system(f'netsh advfirewall firewall add rule name="Block_{ip}" dir=in action=block remoteip={ip}')
-    print(f"🚫 Blocked IP: {ip}")
-
-def unblock_ip(ip):
-    os.system(f'netsh advfirewall firewall delete rule name="Block_{ip}"')
-    print(f"✅ Unblocked IP: {ip}")
